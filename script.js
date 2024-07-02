ccd5493801bbdb03243824075aaa36db907524b2
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-07-02 11:33:40 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-//your JS code here. If required.
-let saveInput = document.querySelector('input[type="submit"]')
-saveInput.addEventListener("click",()=>{
-	let fontSize = document.querySelector('#fontsize').value
-	let fontColor = document.querySelector('input[type="color"]').value
-	// console.log(fontSize,fontColor)
-	document.cookie = "fontcolor="+fontColor+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
-	document.cookie = "fontsize="+fontSize+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
-}) 
+// script.js
 
-function showCookieValue(){
-let fontSizeCookie = getCookie("fontsize")
-	if(fontSizeCookie){
-		document.querySelector('#fontsize').value = fontSizeCookie 
-	}
-	let fontColorCookie =getCookie("fontcolor")
-	if(fontColorCookie){
-		document.querySelector('input[type="color"]').value = fontColorCookie 
-	}
-
-	
+// Function to set cookie
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie(key){
-	let cookies =  document.cookie.split("; ")
-	 .find((row) => row.startsWith(key))
-	if(cookies){
-		return cookies.split("=")[1]
-	}else{
-		return undefined
-	}
-	
+// Function to get cookie
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
+
+// Load preferences from cookies
+window.onload = function() {
+  let fontsize = getCookie("fontsize");
+  let fontcolor = getCookie("fontcolor");
+  if (fontsize && fontcolor) {
+    document.body.style.fontSize = fontsize + "px";
+    document.body.style.color = fontcolor;
+  }
+};
+
+// Save preferences to cookies
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let fontsize = document.getElementById("fontsize").value;
+  let fontcolor = document.getElementById("fontcolor").value;
+  setCookie("fontsize", fontsize, 30);
+  setCookie("fontcolor", fontcolor, 30);
+  document.body.style.fontSize = fontsize + "px";
+  document.body.style.color = fontcolor;
+});
